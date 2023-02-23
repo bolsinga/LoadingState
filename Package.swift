79c566f5bd6fcea5954bc1e63f3314b038167736
Greg Bolsinga <bolsinga@mac.com>
2023-02-23 14:19:42 -0800

    add ios support to package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "LoadingState",
-  platforms: [.macOS(.v13)],
+  platforms: [.macOS(.v13), .iOS(.v16)],
   products: [.library(name: "LoadingState", targets: ["LoadingState"])],
   targets: [.target(name: "LoadingState")]
 )
